f97dd2146f7a9789a68fcada1d467d2db9a592ac
hj <hoji0207@icloud.com>
2022-03-28 17:27:11 +0900

    first commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by hj on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
